10bbeb3406ca12d6a8254e959bdfd5794cb8677d
Eric van Zanten <eric.vanzanten@gmail.com>
2013-07-14 15:54:08 -0500

    Now loading revenue data as part of loading TIF Districts

--- illuminator/management/commands/LoadTifDistricts.py
+++ illuminator/management/commands/LoadTifDistricts.py
@@ -5,6 +5,7 @@ from django.contrib.gis.gdal import DataSource, OGRGeomType, OGRGeometry
 from illuminator.models import TifDistrict
 import os.path
 import json
+import csv
 from datetime import datetime
 from utils import verify_geom
 
@@ -28,6 +29,16 @@ class Command(BaseCommand):
         }
         datafile = os.path.join(os.path.curdir,'data/shp/tifs/TIF_Districts.shp')
         xref = open(os.path.join(os.path.curdir, 'data/xref.json'), 'rb')
+        revenue = {}
+        with open(os.path.join(os.path.curdir, 'data/csv/REVENUE.csv'), 'rb') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                if row.get('TIF'):
+                    tif_id = 'T-%s' % row.get('TIF').zfill(3)
+                    revenue[tif_id] = {
+                        'revenue2010': row.get('REV2010'),
+                        'revenue2011': row.get('REV2011'),
+                    }
         tif_id_ref = json.loads(xref.read())
         data_layer = DataSource(datafile)[0]
         for feat in data_layer:
@@ -42,6 +53,9 @@ class Command(BaseCommand):
                     f[mapping.get(k)] = feat.get(k)
             f['geom'] = verify_geom(feat.geom, TifDistrict._meta.get_field('geom'))
             f['ref_number'] = tif_id_ref[f['name']]
+            rev = revenue.get(f['ref_number'])
+            if rev:
+                f.update(rev)
             tif = TifDistrict(**f)
             tif.save()
 
